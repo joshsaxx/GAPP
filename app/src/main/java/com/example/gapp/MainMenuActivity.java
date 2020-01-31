3c74d3f0615aa8452e50e0aff021fd854db682b6
Joshua Nii Kwei Annan <jannan@IM-21-B1-031.local>
2020-01-31 17:21:33 +0000

    upload changes to dev branch

--- app/src/main/java/com/example/gapp/MainMenuActivity.java
+++ app/src/main/java/com/example/gapp/MainMenuActivity.java
@@ -1,7 +1,9 @@
 package com.example.gapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -21,10 +23,44 @@ public class MainMenuActivity extends AppCompatActivity {
     }
 
     public void showHistory(View view) {
-        startActivity(new Intent(this,SecretPinActivity.class));
+        AlertDialog.Builder a_builder = new AlertDialog.Builder(MainMenuActivity.this);
+        a_builder.setMessage("Incorrect username or password!")
+                .setCancelable(false)
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        showSP();
+                    }
+                });
+        AlertDialog alert =a_builder.create();
+        alert.setTitle("Error!!");
+        alert.show();
+        //startActivity(new Intent(this,SecretPinActivity.class));
     }
 
     public void showStations(View view) {
         startActivity(new Intent(this,InputDenomActivity.class));
+    }
+
+    public void showSP(){
+        final View view = View.inflate(getBaseContext(),R.layout.activity_secret_pin,null);
+        AlertDialog.Builder a_builder = new AlertDialog.Builder(MainMenuActivity.this);
+        a_builder
+                .setView(view)
+                .setCancelable(false)
+
+                .setPositiveButton("Done", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        //startActivity(new Intent(MainMenuActivity.this,MainMenu.class));
+
+                    }
+                })
+                .setNegativeButton("Cancel", null);
+
+        AlertDialog alert =a_builder.create();
+        //alert.setTitle("Create Secret Pin");
+        alert.show();
     }
 }
